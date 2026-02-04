973c66fa74505cdbeabb4643971fba27ca655882
Vladimir Sadovnikov <sadko4u@gmail.com>
2026-02-04 23:41:35 +0300

    Using new mechanism for registering plugin metadata

--- src/main/meta/chorus.cpp
+++ src/main/meta/chorus.cpp
@@ -1,6 +1,6 @@
 /*
- * Copyright (C) 2025 Linux Studio Plugins Project <https://lsp-plug.in/>
- *           (C) 2025 Vladimir Sadovnikov <sadko4u@gmail.com>
+ * Copyright (C) 2026 Linux Studio Plugins Project <https://lsp-plug.in/>
+ *           (C) 2026 Vladimir Sadovnikov <sadko4u@gmail.com>
  *
  * This file is part of lsp-plugins-chorus
  * Created on: 23 мар 2024 г.
@@ -20,6 +20,7 @@
  */
 
 #include <lsp-plug.in/plug-fw/meta/ports.h>
+#include <lsp-plug.in/plug-fw/meta/registry.h>
 #include <lsp-plug.in/shared/meta/developers.h>
 #include <private/meta/chorus.h>
 
@@ -358,6 +359,7 @@ namespace lsp
             mono_plugin_port_groups,
             &chorus_bundle
         };
+        LSP_REGISTER_METADATA(chorus_mono);
 
         const plugin_t chorus_stereo =
         {
@@ -388,6 +390,8 @@ namespace lsp
             stereo_plugin_port_groups,
             &chorus_bundle
         };
+        LSP_REGISTER_METADATA(chorus_stereo);
+
     } /* namespace meta */
 } /* namespace lsp */
 
